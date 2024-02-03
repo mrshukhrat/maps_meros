5938138dbb7402ab9a8bdb7650c83e07cb04975a
shuukhratbegmatov <shuukhratbegmatov@gmail.com>
2024-02-04 03:08:40 +0500

    fix:

---
+++ precache-manifest.00b25ef43356666c4171c5d32e11b23a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f53cdb22ac58e36a9a5",
+    "url": "/css/app.4a04a02b.css"
+  },
+  {
+    "revision": "866d16f86dacbb087c62",
+    "url": "/css/chunk-vendors.5b74f230.css"
+  },
+  {
+    "revision": "1bb0a9a05591a1030908b59f7de4f7ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9c14e0fe02b05ca63d3",
+    "url": "/js/about.59445392.js"
+  },
+  {
+    "revision": "3f53cdb22ac58e36a9a5",
+    "url": "/js/app.a9f737b7.js"
+  },
+  {
+    "revision": "866d16f86dacbb087c62",
+    "url": "/js/chunk-vendors.ef8d053d.js"
+  },
+  {
+    "revision": "50201ad7c7e75ce0593eb1e1c2c9cb87",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
